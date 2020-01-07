09ef488940c4bff6334be22e6c1d845f89c2908c
Rob <robert.j1450@gmail.com>
2020-01-07 17:03:19 -0500

    basic query string setup

--- src/app.js
+++ src/app.js
@@ -44,6 +44,34 @@ app.get('/help', (req, res) => {
     })
 })
 
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+
+    res.send({
+        forecast: 'it is snowing out',
+        location: 'Philadelphia',
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    } 
+
+    console.log(req.query.search)
+
+    res.send({
+        products: []
+    })
+})
+
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
