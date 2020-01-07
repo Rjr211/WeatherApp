490661252c68a4c87bbe48be1736d639932033cc
Rob <robert.j1450@gmail.com>
2020-01-07 18:13:54 -0500

    Added geocode, forecast, utils, and search params

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express') 
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 
 //Call express
@@ -49,14 +51,33 @@ app.get('/weather', (req, res) => {
         return res.send({
             error: 'You must provide an address'
         })
-    }
+    } 
 
-    res.send({
-        forecast: 'it is snowing out',
-        location: 'Philadelphia',
-        address: req.query.address
+    
+    geocode(req.query.address, (error, {lat, long, location} = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(lat, long, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
+
+
+
+
+
 })
+
 
 app.get('/products', (req, res) => {
     if (!req.query.search) {
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (lat, long, callback) => {
+    const url = 'https://api.darksky.net/forecast/613560527af832f0d4de28328f91e8d9/' + lat + ',' + long
+    request({url:url, json:true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to location services', undefined)
+        } else if (body.error){
+            callback('Incorrect search term, try again', undefined)
+        } else {
+            callback(undefined, 'It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + ' chance of rain')
+        }
+    })
+
+}
+
+module.exports = forecast
