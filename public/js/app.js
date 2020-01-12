a0cc0df5ef7372df5252128be6607a80ee4c3d80
Rob <robert.j1450@gmail.com>
2020-01-12 13:57:28 -0500

    error handling on invalid location, added weather to page on search

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@ console.log('Client side JavaScript has loaded')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 
 weatherForm.addEventListener('submit', (e) => {
@@ -9,14 +11,21 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value
 
+    messageOne.textContent = 'Loading..'
+    messageTwo.textContent = ''
+
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        
 
         response.json().then((data) => {
             if(data.error){
-                console.log(data.error)
+                messageOne.style.color = 'red'
+                messageOne.textContent = data.error
             } else {
-                console.log(data.location)
-                console.log(data.forecast)
+                messageOne.style.color = 'green'
+                messageOne.textContent = data.location
+                messageTwo.style.color = 'green'
+                messageTwo.textContent = data.forecast
             }
             
         })
