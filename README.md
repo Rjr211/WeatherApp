6266388241842fa2fd6e6d229a0c245477590ad8
Rob <robert.j1450@gmail.com>
2020-01-19 23:51:09 -0500

    added text to about, fixed image

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 * This weather application will display the current weather, forecast, and location on Google Maps depending on what the user searches.
 
-* In addition, if the user is interested there is an about me page which talks a little bit about the developer (me), and if the user runs into any issues make sure to visit the help page!
+* In addition, there is an about page which talks a little bit about website and if the user runs into any issues make sure to visit the help page!
 
 * Technologies: HTML, CSS, Handlebars, Node, Express, Dark Sky API, Fetch API, Google Maps API
 
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,10 +8,16 @@ const forecast = (lat, long, callback) => {
         } else if (body.error){
             callback('Incorrect search term, try again', undefined)
         } else {
-            callback(undefined, 'It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + ' chance of rain')
+            callback(undefined, 'It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + ' chance of rain' + 
+            
+            body.currently.icon)
         }
+
     })
 
 }
+
+
+
 
 module.exports = forecast
